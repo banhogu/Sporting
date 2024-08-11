f4c10e27b9782df1b16456015a0fcc63cb3157cd
hojin <devbang5674@gmail.com>
2024-08-11 19:18:37 +0900

    feat: 도메인 연결

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://spoting-banhogus-projects.vercel.app/'
+      url: 'https://www.spoting.site'
     }
   ];
 }
