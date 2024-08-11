e6dfeb828998000798342711e816fc4514bd47b8
hojin <devbang5674@gmail.com>
2024-08-11 18:18:43 +0900

    fix: sitemap 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://spoting-2oyi.vercel.app/'
+      url: 'https://spoting-banhogus-projects.vercel.app/'
     }
   ];
 }
